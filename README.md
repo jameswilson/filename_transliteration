afa613af40e8c3f6fd02a8acfca1e430f60df757
James Wilson <jrwilson3@gmail.com>
2017-03-26 12:47:09 -0400

    Filename transliteration for Drupal 8

---
+++ README.md
@@ -0,0 +1,29 @@
+# Filename Transliteration
+
+A Drupal 8 helper module to enable basic transliteration for all uploaded
+File Entities.
+
+Transliterations include:
+
+* Special characters are converted from UTF8 to ASCII using Drupal core's
+  [PhpTransliteration::transliterate][1] functionality.
+* Capital leters are lowercased.
+* Anything that is not a valid filename character (including the space
+  character) is repleaced with an underscore.
+* Sequences of underscores, dashes, and periods are simplified.
+
+## Why does this module exist?
+
+While the transliteration functionality was ported into Drupal 8 core, there is no support for filename transliteration yet.
+
+## Configuration
+
+The module currently has no configuration UI. Pull Requests welcome.
+
+## Credits
+
+This module is based on the [blog post][2] by Alexander Belov, but
+simplified for a less specific use case.
+
+[1]: https://api.drupal.org/api/function/PhpTransliteration::transliterate
+[2]: https://www.buzzwoo.de/blog/better-filename-transliteration-drupal-8-transliteration-using-non-default-language
---
+++ src/FilenamePostprocessor.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\filename_transliteration;
+
+use \Drupal\Core\Config\ConfigFactoryInterface;
+use \Drupal\Component\Transliteration\TransliterationInterface;
+use \Drupal\Component\Utility\Unicode;
+
+/**
+ * Class FilenamePostprocessor.
+ *
+ * @package Drupal\filename_transliteration
+ */
+class FilenamePostprocessor {
+
+  /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+
+  /**
+   * The transliteration service.
+   *
+   * @var \Drupal\Component\Transliteration\PhpTransliteration
+   */
+  protected $transliteration;
+
+  /**
+   * Constructs the Filename Postprocessor service.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   * @param \Drupal\Component\Transliteration\TransliterationInterface $transliteration
+   *   The transliteration service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, TransliterationInterface $transliteration) {
+    $this->configFactory = $config_factory;
+    $this->transliteration = $transliteration;
+  }
+
+  /**
+   * Custom method for transliterating filenames converts
+   * spaces to underscores and passes the filename through
+   * Drupal's core transliteration mechanism.
+   * @param String $filename
+   *   The filename containing characters.
+   * @return String
+   *   A transliterated filename with lowercase US ASCII characters only.
+   */
+  public function process($filename) {
+
+    // Transliterate and downcase.
+    $filename = $this->transliteration->transliterate($filename, 'en', '_');
+    $filename = Unicode::strtolower($filename);
+
+    // Repace anything that is not a valid filename character, including a
+    // space, with an underscore.
+    $filename = preg_replace('/[^a-z0-9_.-]+/', '_', $filename);
+
+    // Replace sequential separator characters with a single instance.
+    $filename = preg_replace('/\_+/', '_', $filename);
+    $filename = preg_replace('/\-+/', '-', $filename);
+    $filename = preg_replace('/\.+/', '.', $filename);
+    $filename = str_replace('_-_', '_', $filename);
+    $filename = str_replace('_-', '_', $filename);
+    $filename = str_replace('-_', '_', $filename);
+    $filename = str_replace('_.', '.', $filename);
+    $filename = str_replace('-.', '.', $filename);
+
+    return $filename;
+  }
+}
